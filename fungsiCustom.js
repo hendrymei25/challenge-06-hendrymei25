a8f8a68efe67d91d0e49db137a65abdfbca7aa76
hendrymei25 <70930111+hendrymei25@users.noreply.github.com>
2022-06-11 20:12:52 +0700

    semoga bener

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+var fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,26 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  fs.readFile(file1, "utf8", (err, data) => {
+    if (err) fnCallback(err, null);
+    const { message } = JSON.parse(data);
+    result.push(message.split(" ")[1]);
+    fs.readFile(file2, "utf8", (err, data) => {
+      if (err) fnCallback(err, null);
+      const response = JSON.parse(data);
+      console.log(response[0].message.split(" ")[1]);
+      result.push(response[0].message.split(" ")[1]);
+      fs.readFile(file3, "utf8", (err, data) => {
+        if (err) fnCallback(err, null);
+        const response = JSON.parse(data);
+        console.log(response[0].data.message.split(" ")[1]);
+        result.push(response[0].data.message.split(" ")[1]);
+        fnCallback(null, result);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
